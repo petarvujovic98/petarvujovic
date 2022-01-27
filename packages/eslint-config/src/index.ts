f74ba0db6987a16c6f1579edca08f0d862b62e35
Petar Vujović <petarvujovic98@gmail.com>
2022-01-27 15:14:42 +0100

    feat(eslint-config): Update configuration
    Move configuration into overrides

--- packages/eslint-config/src/index.ts
+++ packages/eslint-config/src/index.ts
@@ -1,34 +1,43 @@
 module.exports = {
-  plugins: ["@typescript-eslint", "node"],
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:import/recommended",
-    "plugin:import/typescript",
-    "prettier",
-  ],
-  settings: {
-    "import/resolver": {
-      typescript: {
-        alwaysTryTypes: true,
+  overrides: [
+    {
+      files: ["*.js", "*.jsx", "*.ts", "*.tsx"],
+      plugins: ["node"],
+      extends: ["eslint:recommended", "plugin:import/recommended", "prettier"],
+      rules: {
+        "no-console": [
+          "error",
+          {
+            allow: ["warn", "error"],
+          },
+        ],
+        "node/no-process-env": "error",
+        "import/order": ["error", { "newlines-between": "always" }],
       },
     },
-  },
-  rules: {
-    "@typescript-eslint/no-unused-vars": [
-      "error",
-      {
-        argsIgnorePattern: "^_",
-        varsIgnorePattern: "^_",
+    {
+      files: ["*.ts", "*.tsx"],
+      plugins: ["@typescript-eslint"],
+      extends: [
+        "plugin:import/typescript",
+        "plugin:@typescript-eslint/recommended",
+      ],
+      settings: {
+        "import/resolver": {
+          typescript: {
+            alwaysTryTypes: true,
+          },
+        },
       },
-    ],
-    "no-console": [
-      "error",
-      {
-        allow: ["warn", "error"],
+      rules: {
+        "@typescript-eslint/no-unused-vars": [
+          "error",
+          {
+            argsIgnorePattern: "^_",
+            varsIgnorePattern: "^_",
+          },
+        ],
       },
-    ],
-    "import/order": ["error", { "newlines-between": "always" }],
-    "node/no-process-env": "error",
-  },
+    },
+  ],
 };
